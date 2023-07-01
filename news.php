06f6dbd57c94ab96a41f73c91f3974bccea1e12b
Kosuke Kamata <kamata@gmail.com>
2023-07-01 13:04:29 +0900

    news.php正常化2。

--- news.php
+++ news.php
@@ -15,9 +15,6 @@ $result = pg_query($dbconn, $sql);
 $NUM = pg_numrows($result);
 ?>
     <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=EUC-JP"/>
-    </head>
     <body BGCOLOR="#FFFFFF" TEXT="#333333" LINK="#258FB8" VLINK="#258FB8" ALINK="#996600" LEFTMARGIN="0" TOPMARGIN="0" MARGINWIDTH="0"
           MARGINHEIGHT="0">
     <table width="490" cellspacing="0" cellpadding="5" border="1" bordercolor="dddddd">
