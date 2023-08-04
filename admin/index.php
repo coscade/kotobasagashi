d32b950f530b0bf683fdafaa7ca1afc865aa0c97
Kosuke Kamata <kamata@gmail.com>
2023-08-05 00:50:26 +0900

    言葉新規登録リンク設置。

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,7 @@
 <?php require_once '../inc/func.inc' ?>
 <?php require_once 'inc/admin_start.inc' ?>
 <h2>過去のことば一覧</h2>
+<a href="/admin/kotoba_1_input.php">新規登録</a>
 <?php
 define('LIST_NUM', 20);
 $P_NUM = isset($_GET['p_num']) ? $_GET['p_num'] : 1;
