921f10496a33f55438bbbd65a129bfe27cb50a1f
kamat <kamata@gmail.com>
2023-08-26 00:19:23 +0900

    今日の「オラクルカード」

--- Carddaytameshi.php
+++ Carddaytameshi.php
@@ -1,200 +1,209 @@
-<?php require_once 'inc/func.inc'; ?>
 <?php
-$CONTENTS_TITLE = "■今日の「オラクルカード■";
-require_once $_SERVER["DOCUMENT_ROOT"] . '/inc/head_set_2column2.inc';
+require_once 'inc/func.inc';
+$CONTENTS_TITLE = "■今日の「オラクルカード」■";
+require_once 'inc/head_set_2column2.inc';
 ?>
+<img src=../img/point_ko.gif alt=width="22" height="17" border="0">
+<strong>
+    <font color="#9400D3">
+        <FONT size="3">
+            みなさまの今日一日の幸せと恵みを祈り、心を込めてひきました。<br>
+            　　今日一日が、恵みの日、感謝の日でありますように…
+        </font>
+</strong>
+
+<HR>
+
+<table width="100%">
+    <tr>
+        <td bgcolor="#cc6699" align="center">
+            <font color="#ffffff">
+                ★今日のカードは
+                <A href="/book/view.php?sid=1771" target="_blank">
+                    「エンジェルオラクルカードII」
+                </A>
+                からです。
+            </font>
+        </td>
+    </tr>
+</table>
+<br><br>
+
+<img src="/img/402110227Zanna.JPG" alt="ANGELS.jpg" border="0" align="left"></a>
+　　
+<p style="color:#B22222;font-weight: bold;font-size: 16px;">
+    ★《Zanna》★　 ザナ
+</p>
+<strong>
+    　　　　　　　　　あなたは全ての危険から<br>
+    　　　　　　　　　守られています。<br>
+    　　　　　　　　　最悪の事態は過ぎ去りました。<br>
+    　　　　　　　　　リラックスして安心して<br>
+    　　　　　　　　　ください。<br>
+    　　　　　　　　　過去にどんなことがあったとしても<br>
+    　　　　　　　　　今この瞬間から未来に向けて、<br>
+    　　　　　　　　　あなたは天使たちに守られて<br>
+    　　　　　　　　　います。<br>
+</strong>
+　
+<br><br><br><br>
+
+<iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=aaaaea00-22&o=9&p=8&l=as1&asins=B001E7IY2Y&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr"
+        style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" border="0"
+        align="left">
+</iframe>
+
+<p style="color:#B22222;font-weight: bold;font-size: 16px;">
+    ★今日のアクションステップ★
+</p>
+
+<p style="color:#9400D3;">
+    天使たちは、私たちの回りにいて、<br>
+    私たちを危険から、低俗なエネルギー、<br>
+    怖れから守ってくれます。<br>
+    だから、安心して前にすすみましょう。<br>
+    自分の回りにいてくれる天使を信じ、<br>
+    怖れず、前に進みましょう。
+</p>
+<br><br>
+
+<p style="color:#9400D3;font-weight: bold;font-size: 14px;">
+    <img src=../img/point_ko.gif alt=width="22" height="17">
+    オラクルカードについてこちらで詳しく紹介しています。
+</p>
+
+<HR>
+
+<A href=/oraclecards.php target=_blank><strong><u>★「オラクルカード」のページにようこそ</A></strong></u><br>
+<A href=/oraclecards.php#deai7 target=_blank><strong><u>★「オラクルカード」の使ってみての他の方々の感想」</A></strong></u><br>
+<A href=/oraclecards.php#deai5 target=_blank><strong><u>★自分に合う「オラクルカード」はどれかな？</A></strong></u><br>
+<A href=http://astore.amazon.co.jp/aaaaea000a5-22/ target=_blank><strong><u>★おすすめの「オラクルカード」一覧</A></strong></u><br>
+<A href=/OriginalCard.php target=_blank><strong><u>★「オリジナルオラクルカード」のつくり方</A></strong></u><br>
+<A href=/oraclecards.php#deai9 target=_blank><strong><u>★※ご相談、サポートについて（有料になります）</A></strong></u><br><br><br>
+
+<p style="color:#9400D3;font-weight: bold;font-size: 14px;">
+    <img src=../img/point_ko.gif alt=width="22" height="17">
+    オラクルカードでおすすめされているグッズ紹介
+</p>
+
+<HR>
+
+　オラクルカードは、アロマの香り、フラワーレメディやハーブティを味わう、<br>　
+クリスタルのものを身につけるなど、おすすめされていることがあります。<br>
+　私も持っているいくつかの商品やショップを紹介いたしますね。<br>
+　アロマの香りは、気持ちをやわらげてくれますし、フラワーレメディ、<br>　
+ハーブティは体をあたため、落ち着かせてくれますし、クリスタルなものを<br>　
+身につけると、身を守るための光を集め、自分を輝かせてくれる、ということです。<br>
+　（写真をクリックしてみてくださいね）<br><br><br>
+
+<p style="font-weight: bold;color: #57BF3F">
+    ★クリスタルペンダントヘッド<br>
+    　キラキラしてキレイで気に入っています。色違いもあります。　
+</p>
+<br><br>
+
+<a href="http://ls.j-a-net.jp/?a=1353002&d=263225&url=http%3A%2F%2Fshop.cafeglobe.com%2Fdetail%2F%3FGoods%3D10029%26Color%3D636&u="
+   target="_blank">
+    <IMG SRC="http://shop.cafeglobe.com/goodsphoto/10029/636-1_large_detail_pc.jpg">
+</a>
+<br><br><br>
+
+<p style="font-weight: bold;color: #57BF3F">
+    ★スワロフスキーのクリスタルペンダント<br>
+    　気分がすぐれないときには、このペンダントいいですよ。<br>
+    　プレゼントにもおすすめです。
+</p>
+<br><br>
+
+<a href="http://ls.j-a-net.jp/?a=1353002&d=7846&url=http%3A%2F%2Fredirect.shoppingfeed.jp%2Fjewelshop.xm%2Fsp_redirects%2F%3Fpid%3D2000%26sid%3D0400%26icd%3D127&u="
+   target="_blank">
+    <IMG SRC="http://image1.shopserve.jp/e-jewelshop.com/pic-labo/limg/R00294_2.jpg" border="0">
+</a>
+<br><br><br>
+
+<p style="font-weight: bold;color: #57BF3F">
+    ★クリスタルブレス<br>
+    　とてもキレイですよ～。<br>
+    　美しいものをすると身が引き締まりますね。
+</p>
+<br><br>
+
+<a href="http://ls.j-a-net.jp/?a=1353002&d=8244&url=http%3A%2F%2Fredirect.shoppingfeed.jp%2Fsarihati.th%2Fsp_redirects%2F%3Fpid%3D2000%26sid%3D0400%26icd%3DAGEN-BA-CRS6&u="
+   target="_blank">
+    <img SRC="http://image1.shopserve.jp/sarihati.th.shopserve.jp/pic-labo/limg/Ba_CrystalMoon_1_600550.jpg">
+</a>
+<br><br><br>
+
+<p style="font-weight: bold;color: #57BF3F">
+    ★天然植物100％の最高級オイル配合！<br>
+    　「親水性乳化アロマエッセンシャルオイル<br>
+    　肌にも直接つけられるタイプのアロマオイルです。
+</p>
+<br><br>
+
+<a href="http://ls.j-a-net.jp/?a=1353002&d=7846&url=http%3A%2F%2Fredirect.shoppingfeed.jp%2Fcoco.by%2Fsp_redirects%2F%3Fpid%3D2000%26sid%3D0400%26icd%3DIN-1095-W&u="
+   target="_blank">
+    <IMG SRC="http://image1.shopserve.jp/zakkaya-coco.com/pic-labo/limg/eb074item.jpg" border="0">
+</a>
+<br><br><br>
+
+<p style="font-weight: bold;color: #57BF3F">
+    ★人気のアロマオイル<br>
+    これから冬にかけては、ゼラニウム、カモマイルなどを<br>
+    ゆったりと使いたいところです。
+</p>
+<br><br>
+<a href="http://px.a8.net/svt/ejp?a8mat=1O5FDF+5O7P9U+JO2+ZPSKX&a8ejpredirect=http%3A%2F%2Fwww.herbtea-store.com%2Fi_vv%2Flist%2Foil.html"
+   target="_blank">
+    <img border="0" alt="" src="http://www.herbtea-store.com/i_vv/e_oil/mm1.jpg">
+</a>
+<img border="0" width="1" height="1" src="http://www14.a8.net/0.gif?a8mat=1O5FDF+5O7P9U+JO2+ZPSKX" alt="">
+<br><br><br>
+
+<p style="font-weight: bold;color: #57BF3F">
+    ★バッジフラワーレメディ…<br>
+    　植物の力を借りて、ネガティブな感情（不安や緊張、嫉妬、恐れなど）の<br>
+    　バランスをとりもどす自然のヒーリング方法で、水に容れて飲みます。
+</p>
+<br><br>
+<a href="http://px.a8.net/svt/ejp?a8mat=1O5ELM+CULTTE+DMU+TRGJ5&a8ejpredirect=http%3A%2F%2Fwww.suplinx.com%2Fcolumn%2Fbach.html"
+   target="_blank">
+    <img border="0" alt="" src="http://www.suplinx.com/column/images/bach23.jpg">
+</a>
+<img border="0" width="1" height="1" src="http://www17.a8.net/0.gif?a8mat=1O5ELM+CULTTE+DMU+TRGJ5" alt="">
+<br><br><br>
 
 
-<style type="text/css"><!--
+<p style="font-weight: bold;color: #57BF3F">
+    ★ハーブティ…ホッとしますよ。<br>
+    　こちらのものは、ぼちぼちでも紹介しました。↓<br>
+    <a href="http://www.enpitu.ne.jp/usr10/bin/day?id=104435&pg=20060315" target="_blank">
+        末期ガンを治した「奇跡のハーブティー」
+    </a>
+</p>
 
-    div#kantanni {
-        width: 100%;
+<p style="font-weight: bold;color: #57BF3F">
+    　書籍紹介は→
+    <a href="/book/view.php?sid=1135" target="_blank">
+        「奇跡のハーブティー」
+    </a>
+</p>
 
-        margin: 0;
-        padding: 6px;
-        text-align: left;
-        background: #EFFDE6;
-        border-left: 1px solid #C1DF7D;
-        border-top: 1px solid #C1DF7D;
-        border-right: 1px solid #95AD5E;
-        border-bottom: 1px solid #95AD5E;
-    }
+<a href="http://px.a8.net/svt/ejp?a8mat=1O5ELM+CULTTE+DMU+TRGJ5&a8ejpredirect=http%3A%2F%2Fwww.suplinx.com%2Fshop%2Fcategory%2Fcategory.aspx%3Fcategory%3DB201"
+   target="_blank">
+    <img border="0" alt="" src="http://www.suplinx.com/img/goods/L/135-00036.jpg"></a>
+<img border="0" width="1" height="1" src="http://www14.a8.net/0.gif?a8mat=1O5ELM+CULTTE+DMU+TRGJ5" alt="">
+<br><br>
 
-    #kihon_litle {
-        font-size: 12px;
-        line-height: 150%;
-    }
+<p style="font-weight: bold;color: #57BF3F">
+    ★ハーブティ…これからの季節は、<br>
+    　レモングラス、ピーチアップル、カモマイルなどいいですよ～
+</p>
+<a href="http://px.a8.net/svt/ejp?a8mat=1O5FDF+5O7P9U+JO2+ZPSKX&a8ejpredirect=http%3A%2F%2Fwww.herbtea-store.com%2F"
+   target="_blank">
+    <img src="http://www.herbtea-store.com/i_vv/herb_tea/img/t1.jpg">
+</a>
 
-
-    --></style>
-
-
-<div ><br>
-
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><strong><font color=9400D3><FONT size="3">みなさまの今日一日の幸せと恵みを祈り、心を込めてひきました。<br>
-                　　今日一日が、恵みの日、感謝の日でありますように…</font></strong>
-    <HR COLOR=9400D3>
-    </font><br>
-
-
-    <table border="0"   width=480>
-        <tr>
-            <td bgcolor=#cc6699 align=center >
-                <B><font color=#ffffff><strong>★今日のカードは<A href=/book/view.php?sid=1771 target=_blank><strong><u>「エンジェルオラクルカードII」</A></strong></u>
-                        からです。
-                        <br></strong></B></font>
-            </td>
-        </tr>
-    </table>
-    <br><br>
-
-
-    <img src="/img/402110227Zanna.JPG" alt="ANGELS.jpg" border="0" align="left"></a>
-
-
-    　　
-    <strong><font color=B22222><FONT size="3">　　　★《Zanna》★　<br>　　　　　　　ザナ
-                　　　　　　</strong></font></font>　<br>　<br>
-
-    　　　<strong>　 　　 　　あなたは全ての危険から<br>
-        　　　　　　　　　守られています。<br>
-        　　　　　　　　　最悪の事態は過ぎ去りました。<br>
-        　　　　　　　　　リラックスして安心して<br>
-        　　　　　　　　　ください。<br>
-        　　　　　　　　　過去にどんなことがあったとしても<br>
-        　　　　　　　　　今この瞬間から未来に向けて、<br>
-        　　　　　　　　　あなたは天使たちに守られて<br>
-        　　　　　　　　　います。<br>
-
-
-        　　　　　　　　　　　　　　　　</strong>
-
-    　
-    <br><br><br><br>　<br><br>
-
-    <iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=aaaaea00-22&o=9&p=8&l=as1&asins=B001E7IY2Y&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr"
-            style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0" border="0"
-            align="left"></iframe>
-
-
-    　　　　　　　　　　　　　　<strong><font color=B22222><FONT size="3">★今日のアクションステップ★<br><br></strong></font></font>
-
-    　　　　　　　　　　　　　天使たちは、私たちの回りにいて、<br>
-    　　　　　　　　　　　　　私たちを危険から、低俗なエネルギー、<br>
-    　　　　　　　　　　　　　怖れから守ってくれます。<br>
-    　　　　　　　　　　　　　だから、安心して前にすすみましょう。<br>
-    　　　　　　　　　　　　　自分の回りにいてくれる天使を信じ、<br>
-    　　　　　　　　　　　　　怖れず、前に進みましょう。<br><br>
-
-
-    </font></strong>
-    <br clear="all"><br>
-
-    <br>
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><font color=9400D3><strong><a name="deai8"><FONT
-                        size="2">オラクルカードについてこちらで詳しく紹介しています。</A></font></font>
-    <HR COLOR=9400D3>
-    </strong>　<br>
-    　<A href=/oraclecards.php target=_blank><strong><u>★「オラクルカード」のページにようこそ</A></strong></u><br>
-    　<A href=/oraclecards.php#deai7 target=_blank><strong><u>★「オラクルカード」の使ってみての他の方々の感想」</A></strong></u><br>
-    　<A href=/oraclecards.php#deai5 target=_blank><strong><u>★自分に合う「オラクルカード」はどれかな？</A></strong></u><br>
-    　<A href=http://astore.amazon.co.jp/aaaaea000a5-22/ target=_blank><strong><u>★おすすめの「オラクルカード」一覧</A></strong></u>
-    <br>
-    　<A href=/OriginalCard.php target=_blank><strong><u>★「オリジナルオラクルカード」のつくり方</A></strong></u><br>
-    　<A href=/oraclecards.php#deai9 target=_blank><strong><u>★※ご相談、サポートについて（有料になります）</A></strong></u><br><br><br>
-
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><font color=9400D3><strong><a name="deai8"><FONT
-                        size="2">オラクルカードでおすすめされているグッズ紹介</A></font></font>
-    <HR COLOR=9400D3>
-    </strong>　<br>
-
-    　オラクルカードは、アロマの香り、フラワーレメディやハーブティを味わう、<br>　クリスタルのものを身につけるなど、おすすめされていることがあります。<br>
-    　私も持っているいくつかの商品やショップを紹介いたしますね。<br>
-    　アロマの香りは、気持ちをやわらげてくれますし、フラワーレメディ、<br>　ハーブティは体をあたため、落ち着かせてくれますし、クリスタルなものを<br>　身につけると、身を守るための光を集め、自分を輝かせてくれる、ということです。<br>
-    　（写真をクリックしてみてくださいね）<br>　<br><br>
-
-
-    <strong><font color=#57BF3F>★クリスタルペンダントヘッド<br>
-            　キラキラしてキレイで気に入っています。色違いもあります。　</font></strong><br><br>
-
-    <a href="http://ls.j-a-net.jp/?a=1353002&d=263225&url=http%3A%2F%2Fshop.cafeglobe.com%2Fdetail%2F%3FGoods%3D10029%26Color%3D636&u="
-       target="_blank"><IMG SRC="http://shop.cafeglobe.com/goodsphoto/10029/636-1_large_detail_pc.jpg"
-                            border="0"></a><br><br><br>
-
-
-    <strong><font color=#57BF3F>★スワロフスキーのクリスタルペンダント<br>
-            　気分がすぐれないときには、このペンダントいいですよ。<br>
-            　プレゼントにもおすすめです。</font></strong><br><br>
-    <a href="http://ls.j-a-net.jp/?a=1353002&d=7846&url=http%3A%2F%2Fredirect.shoppingfeed.jp%2Fjewelshop.xm%2Fsp_redirects%2F%3Fpid%3D2000%26sid%3D0400%26icd%3D127&u="
-       target="_blank"><IMG SRC="http://image1.shopserve.jp/e-jewelshop.com/pic-labo/limg/R00294_2.jpg"
-                            border="0"></a><br><br><br>
-
-
-    <strong><font color=#57BF3F>★クリスタルブレス<br>
-            　とてもキレイですよ～。<br>
-            　美しいものをすると身が引き締まりますね。</font></strong><br><br>
-    <a href="http://ls.j-a-net.jp/?a=1353002&d=8244&url=http%3A%2F%2Fredirect.shoppingfeed.jp%2Fsarihati.th%2Fsp_redirects%2F%3Fpid%3D2000%26sid%3D0400%26icd%3DAGEN-BA-CRS6&u="
-       target="_blank"><IMG
-                SRC="http://image1.shopserve.jp/sarihati.th.shopserve.jp/pic-labo/limg/Ba_CrystalMoon_1_600550.jpg"
-                border="0"></a><br><br><br>
-
-
-    <strong><font color=#57BF3F>★天然植物100％の最高級オイル配合！<br>
-            　「親水性乳化アロマエッセンシャルオイル<br>
-            　肌にも直接つけられるタイプのアロマオイルです。</font></strong><br><br>
-
-    <a href="http://ls.j-a-net.jp/?a=1353002&d=7846&url=http%3A%2F%2Fredirect.shoppingfeed.jp%2Fcoco.by%2Fsp_redirects%2F%3Fpid%3D2000%26sid%3D0400%26icd%3DIN-1095-W&u="
-       target="_blank"><IMG SRC="http://image1.shopserve.jp/zakkaya-coco.com/pic-labo/limg/eb074item.jpg"
-                            border="0"></a><br><br><br>
-
-    <strong><font color=#57BF3F>★人気のアロマオイル<br>
-            　これから冬にかけては、ゼラニウム、カモマイルなどを<br>
-            　ゆったりと使いたいところです。</font></strong><br><br>
-    <a href="http://px.a8.net/svt/ejp?a8mat=1O5FDF+5O7P9U+JO2+ZPSKX&a8ejpredirect=http%3A%2F%2Fwww.herbtea-store.com%2Fi_vv%2Flist%2Foil.html"
-       target="_blank">
-        <img border="0" alt="" src="http://www.herbtea-store.com/i_vv/e_oil/mm1.jpg"></a>
-    <img border="0" width="1" height="1" src="http://www14.a8.net/0.gif?a8mat=1O5FDF+5O7P9U+JO2+ZPSKX"
-         alt=""><br><br><br>
-
-
-    <strong><font color=#57BF3F>★バッジフラワーレメディ…<br>
-            　植物の力を借りて、ネガティブな感情（不安や緊張、嫉妬、恐れなど）の<br>
-            　バランスをとりもどす自然のヒーリング方法で、水に容れて飲みます。</font></strong><br><br>
-    <a href="http://px.a8.net/svt/ejp?a8mat=1O5ELM+CULTTE+DMU+TRGJ5&a8ejpredirect=http%3A%2F%2Fwww.suplinx.com%2Fcolumn%2Fbach.html"
-       target="_blank">
-        <img border="0" alt="" src="http://www.suplinx.com/column/images/bach23.jpg"></a>
-    <img border="0" width="1" height="1" src="http://www17.a8.net/0.gif?a8mat=1O5ELM+CULTTE+DMU+TRGJ5"
-         alt=""><br><br><br>
-
-
-    <strong><font color=#57BF3F>★ハーブティ…ホッとしますよ。<br>
-            　こちらのものは、ぼちぼちでも紹介しました。↓<br>
-            　<A href=http://www.enpitu.ne.jp/usr10/bin/day?id=104435&pg=20060315 target=_blank><strong><u>末期ガンを治した「奇跡のハーブティー」</A></strong></u></A>
-    <br>
-    　書籍紹介は→<A href=/book/view.php?sid=1135 target=_blank><strong><u>「奇跡のハーブティー」</A></strong></u>
-    　</font></strong><br><br>
-
-    <a href="http://px.a8.net/svt/ejp?a8mat=1O5ELM+CULTTE+DMU+TRGJ5&a8ejpredirect=http%3A%2F%2Fwww.suplinx.com%2Fshop%2Fcategory%2Fcategory.aspx%3Fcategory%3DB201"
-       target="_blank">
-        <img border="0" alt="" src="http://www.suplinx.com/img/goods/L/135-00036.jpg"></a>
-    <img border="0" width="1" height="1" src="http://www14.a8.net/0.gif?a8mat=1O5ELM+CULTTE+DMU+TRGJ5"
-         alt=""><br><br><br>
-
-
-    <strong><font color=#57BF3F>★ハーブティ…これからの季節は、<br>
-            　レモングラス、ピーチアップル、カモマイルなどいいですよ～<strong><font color=#57BF3F><br><br>
-                    <a href="http://px.a8.net/svt/ejp?a8mat=1O5FDF+5O7P9U+JO2+ZPSKX&a8ejpredirect=http%3A%2F%2Fwww.herbtea-store.com%2F"
-                       target="_blank">
-                        <img border="0" alt="" src="http://www.herbtea-store.com/i_vv/herb_tea/img/t1.jpg"></a>
-                    <img border="0" width="1" height="1" src="http://www19.a8.net/0.gif?a8mat=1O5FDF+5O7P9U+JO2+ZPSKX"
-                         alt="">
-
-
-                    　　　　　　　　　　　　　　　　　　　　　　　
-
-</div>
-</div>
-</div>
-
+<img src="http://www19.a8.net/0.gif?a8mat=1O5FDF+5O7P9U+JO2+ZPSKX">
 
 <?php require_once $_SERVER["DOCUMENT_ROOT"] . '/inc/foot_set_2column2.inc' ?>
