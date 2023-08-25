63938c508736213801efebcde674bbf929d11725
kamat <kamata@gmail.com>
2023-08-26 00:36:32 +0900

    サイトマップ正常化。

--- map.php
+++ map.php
@@ -1,119 +1,108 @@
-<?php require_once 'inc/func.inc'; ?>
 <?php
+require_once 'inc/func.inc';
 $CONTENTS_TITLE = "■サイトマップ■";
-require_once $_SERVER["DOCUMENT_ROOT"] . '/inc/head_set_2column2.inc';
+require_once 'inc/head_set_2column2.inc';
 ?>
 
-<style type="text/css"><!--
+<img src="/img/point_ko.gif" width="22" height="17">
+<B><font color="#3FBF57">～いらっしゃいませ、どうぞ、ごゆっくりしてってくださいね！～</font></B>
+<img src="/img/point_ko.gif" width="22" height="17">
+<br><br>
 
-    div#hajimemashite {
-        width: 450;
-        margin: 0;
-        padding: 6px;
-        text-align: left;
-        background-image: url(/img/kotoba_about_bg.jpg);
-        background-repeat: no-repeat;
-        background: #EFFDE6;
-        border-left: 1px solid #C1DF7D;
-        border-top: 1px solid #C1DF7D;
-        border-right: 1px solid #95AD5E;
-        border-bottom: 1px solid #95AD5E;
-    }
+<img src="/img/point_ko.gif" width="22" height="17">
+<B>「今日のことば」コンテンツ</B>
+<img src="/img/point_ko.gif" width="22" height="17">
+<br><br>
+　　　┣<a href="/">今日のことばＴＯＰ　（毎日ＵＰ）</a><br>
+　　　┣<a href="http://www.enpitu.ne.jp/usr10/104435/diary.html">まゆのぼちぼち日記　（金・土曜日、忙しい時、連休はお休み）</a><br>
+　　　┣<a href="http://www1.rocketbbs.com/313/kotoba.html">ことば探しBBS </a><br>
+　　　┣<a href="/afm.php">アファメーションについて</a><br>
+　　　┣<a href="/about.php">このページについて</a><br>
+　　　┣<a href="/owner.php">主催者「まゆ」について</a><br>
+　　　┣<a href="/mm.php">メルマガ登録と解除</a><br>
+　　　┣<a href="/link.php">リンク集</a><br>
+　　　┗<a href="/mail.php?title=%A4%AA%CC%E4%B9%E7%A4%BB%A4%CF%A4%B3%A4%C1%A4%E9">主催へのお問合せ･主催者まゆへのメール</a><br><br>
 
+<img src="/img/point_ko.gif" width="22" height="17">
+<B>必見！本から読みとく「男女の違い」</B>
+<img src="/img/point_ko.gif" width="22" height="17">
+<br><br>
+　　～知っているか、いないかで全然違う男女の関係」～<br>
+　　　┣<a href="/danjyo/weekly.php">「今月の「男女の違い」　（ちょっとお休みしています。すいません。）</a><br>
+　　　┣<a href="/danjyo/01.php">「男と女の違い」を知ることをおすすめするわけ</a><br>
+　　　┣<a href="/danjyo/books.php">男女の違いが書いてある　「２０冊のおすすめ本」←<strong>おすすめ</strong></a><br>
+　　　┣<a href="/danjyo/table.php">一目でわかる男女の比較表…こんなにあった「男と女の違い３０」←<strong>おすすめ</strong></a><br>
+　　　┗<a href="/danjyo/04.php">女性が気を配るともっと　「うまくいく！２５の行動」←<strong>おすすめ</strong></a><br><br>
 
-    --></style>
+<img src="/img/point_ko.gif" width="22" height="17">
+<B>まゆのお気軽で楽しいぼちぼち的本の読み方＆おすすめ本を紹介</B>
+<img src="/img/point_ko.gif" width="22" height="17">
+<br><br>
+　　　┣<a href="/yomikata/reading.php">今日の「おすすめ本　」(毎日ＵＰ) </a><br>
+　　　┣<a href="/yomikata/01.php">本の読み方</a><br>
+　　　┣<a href="/yomikata/books.php">本の読み方内で紹介している本９０冊…「おすすめ本一覧」</a><br>
+　　　┗<a href="/yomikata/books2.php">夫婦関係を考える…「おすすめ本３２冊」←<strong>おすすめ</strong></a><br><br>
 
+<img src="/img/point_ko.gif" width="22" height="17">
+<B>今日のことば検索とコンテンツ</B>
+<img src="/img/point_ko.gif" width="22" height="17">
+<br><br>
+　　　┣<a href="/kotoba/vest5.php">今月の人気ことばベスト５！　(月１回ＵＰ） </a><br>
+　　　┣<a href="/kotoba/mayubest.php">2005年「今日のことば」まゆのベスト10　(年１回ＵＰ） </a><br>
+　　　┣<a href="/kotoba/view.php">押してみて！今日の「ぴったりことば占い」</a><br>
+　　　┣<a href="/kotoba/calendar.php">日付順で見る（過去のことば） </a><br>
+　　　┗<a href="/kotoba/list.php">全て見る </a><br><br>
 
-<div ><br>
+<img src="/img/point_ko.gif" width="22" height="17">
+<B>今日のことば「探したいカテゴリ」はコチラから</B>
+<img src="/img/point_ko.gif" width="22" height="17">
+<br><br>
+　　　┣<a href="/kotoba/list.php?cmid=1">愛・男女について</a><br>
+　　　┣<a href="/kotoba/list.php?cmid=2">癒し・優しさについて</a><br>
+　　　┣<a href="/kotoba/list.php?cmid=3">人間・人間関係</a><br>
+　　　┣<a href="/kotoba/list.php?cmid=4">仕事・やるべきこと</a><br>
+　　　┣<a href="/kotoba/list.php?cmid=5">人生・生き方について</a><br>
+　　　┣<a href="/kotoba/list.php?cmid=6">自分について</a><br>
+　　　┣<a href="/kotoba/list.php?cmid=7">家族</a><br>
+　　　┣<a href="/kotoba/list.php?cmid=8">社会</a><br>
+　　　┗<a href="/kotoba/list.php?cmid=9">その他 </a><br><br>
 
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><B><font color=#3FBF57>～いらっしゃいませ、どうぞ、ごゆっくりしてってくださいね！～</B></font>
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><br><br>
+<img src="/img/point_ko.gif" width="22" height="17">
+<B>本の検索</B>
+<img src="/img/point_ko.gif" width="22" height="17">
+<br><br>
+　　　┣<a href="/book/list.php?sc=">全て見る</a><br>
+　　　┣<a href="/book/list.php?sc=1">男女・恋愛 </a><br>
+　　　┣<a href="/book/list.php?sc=2">愛・夫婦・結婚</a><br>
+　　　┣<a href="/book/list.php?sc=3">心・心理学、心の病</a><br>
+　　　┣<a href="/book/list.php?sc=4">セルフ・自分を見直す</a><br>
+　　　┣<a href="/book/list.php?sc=5">人間・人間関係</a><br>
+　　　┣<a href="/book/list.php?sc=6">人生・生き方</a><br>
+　　　┣<a href="/book/list.php?sc=7">癒し系 </a><br>
+　　　┣<a href="/book/list.php?sc=8">仕事・マーケティング </a><br>
+　　　┣<a href="/book/list.php?sc=9">ポジティブシンキング </a><br>
+　　　┣<a href="/book/list.php?sc=10">成功法則・夢をかなえる</a><br>
+　　　┣<a href="/book/list.php?sc=11">考え方・ノウハウもの </a><br>
+　　　┣<a href="/book/list.php?sc=12">小説・ファンタジー</a><br>
+　　　┣<a href="/book/list.php?sc=13">病気・体</a><br>
+　　　┣<a href="/book/list.php?sc=14">詩集・哲学 </a><br>
+　　　┣<a href="/book/list.php?sc=15">古典・悟り</a><br>
+　　　┣<a href="/book/list.php?sc=16">エッセイ・ノンフィクション</a><br>
+　　　┣<a href="/book/list.php?sc=17">子供・家族</a><br>
+　　　┣<a href="/book/list.php?sc=18">社会の流れ・全般</a><br>
+　　　┣<a href="/book/list.php?sc=19">ことば集・教訓</a><br>
+　　　┣<a href="/book/list.php?sc=20">その他 </a><br>
+　　　┗<a href="/book/list.php?sc=0">なし</a><br><br>
 
+<img src="/img/point_ko.gif" width="22" height="17">
+<B>募集「ぜひ、教えてください！」</B>
+<img src="/img/point_ko.gif" width="22" height="17">
+<br><br>
+　　　┣
+<a href="/mail.php?title=%A4%A2%A4%CA%A4%BF%A4%CE%A1%D6%A4%AA%A4%B9%A4%B9%A4%E1%A4%CE%CB%DC%A1%D7%A4%F2%B6%B5%A4%A8%A4%C6%A4%AF%A4%C0%A4%B5%A4%A4%A1%A3%3Cbr%3E%A4%E8%A4%ED%A4%B7%A4%B1%A4%EC%A4%D0%CD%FD%CD%B3%A4%E2%B6%B5%A4%A8%A4%C6%A4%AF%A4%C0%A4%B5%A4%A4%A4%CD%A1%A3">あなたの「おすすめの本」</a>
+<br>
+　　　┗
+<a href="/mail.php?title=%A4%A2%A4%CA%A4%BF%A4%CE%A1%D6%BF%CD%C0%B8%A4%F2%CA%D1%A4%A8%A4%BF%CB%DC%A1%D7%A4%F2%B6%B5%A4%A8%A4%C6%A4%AF%A4%C0%A4%B5%A4%A4%A1%A3%3Cbr%3E%A4%E8%A4%ED%A4%B7%A4%B1%A4%EC%A4%D0%BE%DC%A4%B7%A4%AF%B6%B5%A4%A8%A4%C6%A4%AF%A4%C0%A4%B5%A4%A4%A4%CD%A1%A3">あなたの「人生を変えた本」</a>
+<br><br><br>
 
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><B>「今日のことば」コンテンツ</B><img src=../img/point_ko.gif
-                                                                                              alt=width=22 height=17
-                                                                                              border="0"><br><br>
-    　　　┣ <A href="/">今日のことばＴＯＰ　（毎日ＵＰ）</A><br>
-    　　　┣　<A href="http://www.enpitu.ne.jp/usr10/104435/diary.html">まゆのぼちぼち日記　（金・土曜日、忙しい時、連休はお休み）</A><br>
-    　　　┣ <A href="http://www1.rocketbbs.com/313/kotoba.html">ことば探しBBS </A><br>
-    　　　┣　<A href="/afm.php">アファメーションについて</A><br>
-    　　　┣　<A href="/about.php">このページについて</A><br>
-    　　　┣　<A href="/owner.php">主催者「まゆ」について</A><br>
-    　　　┣　<A href="/mm.php">メルマガ登録と解除</A><br>
-    　　　┣　<A href="/link.php">リンク集</A><br>
-    　　　┗　<A href="/mail.php?title=%A4%AA%CC%E4%B9%E7%A4%BB%A4%CF%A4%B3%A4%C1%A4%E9">主催へのお問合せ･主催者まゆへのメール</A><br><br>
-
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><B>必見！本から読みとく「男女の違い」</B><img
-            src=../img/point_ko.gif alt=width=22 height=17 border="0"><br>
-    　　～知っているか、いないかで全然違う男女の関係」～<br><br>
-    　　　┣　<A href="/danjyo/weekly.php">「今月の「男女の違い」　（ちょっとお休みしています。すいません。）</A><br>
-    　　　┣　<A href="/danjyo/01.php">「男と女の違い」を知ることをおすすめするわけ</A><br>
-    　　　┣　<A href="/danjyo/books.php">男女の違いが書いてある　「２０冊のおすすめ本」←<strong>おすすめ</strong></A><br>
-    　　　┣　<A href="/danjyo/table.php">一目でわかる男女の比較表…こんなにあった「男と女の違い３０」←<strong>おすすめ</strong></A><br>
-    　　　┗　<A href="/danjyo/04.php">女性が気を配るともっと　「うまくいく！２５の行動」←<strong>おすすめ</strong></A><br><br>
-
-
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><B>まゆのお気軽で楽しいぼちぼち的本の読み方＆おすすめ本を紹介</B><img
-            src=../img/point_ko.gif alt=width=22 height=17 border="0"><br><br>
-    　　　┣　<A href="/yomikata/reading.php">今日の「おすすめ本　」(毎日ＵＰ) </A><br>
-    　　　┣　<A href="/yomikata/01.php">本の読み方</A><br>
-    　　　┣　<A href="/yomikata/books.php">本の読み方内で紹介している本９０冊…「おすすめ本一覧」</A><br>
-
-    　　　┗　<A href="/yomikata/books2.php">夫婦関係を考える…「おすすめ本３２冊」←<strong>おすすめ</strong></A><br><br>
-
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><B>今日のことば検索とコンテンツ</B><img src=../img/point_ko.gif
-                                                                                                 alt=width=22 height=17
-                                                                                                 border="0"><br><br>
-    　　　┣　<A href="/kotoba/vest5.php">今月の人気ことばベスト５！　(月１回ＵＰ） </A><br>
-    　　　┣　<A href="/kotoba/mayubest.php">2005年「今日のことば」まゆのベスト10　(年１回ＵＰ） </A><br>
-    　　　┣　<A href="/kotoba/view.php">押してみて！今日の「ぴったりことば占い」</A><br>
-    　　　┣　<A href="/kotoba/calendar.php">日付順で見る（過去のことば） </A><br>
-    　　　┗　<A href="/kotoba/list.php">全て見る </A><br><br>
-
-
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><B>今日のことば「探したいカテゴリ」はコチラから</B><img
-            src=../img/point_ko.gif alt=width=22 height=17 border="0"><br><br>
-    　　　┣ <A href="/kotoba/list.php?cmid=1">愛・男女について</A><br>
-    　　　┣ <A href="/kotoba/list.php?cmid=2">癒し・優しさについて</A><br>
-    　　　┣ <A href="/kotoba/list.php?cmid=3">人間・人間関係</A><br>
-    　　　┣ <A href="/kotoba/list.php?cmid=4">仕事・やるべきこと</A><br>
-    　　　┣ <A href="/kotoba/list.php?cmid=5">人生・生き方について</A><br>
-    　　　┣ <A href="/kotoba/list.php?cmid=6">自分について</A><br>
-    　　　┣ <A href="/kotoba/list.php?cmid=7">家族</A><br>
-    　　　┣ <A href="/kotoba/list.php?cmid=8">社会</A><br>
-    　　　┗ <A href="/kotoba/list.php?cmid=9">その他 </A><br><br>
-
-
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><B>本の検索</B><img src=../img/point_ko.gif
-                                                                                      alt=width=22 height=17 border="0"><br><br>
-    　　　┣ <A href="/book/list.php?sc=">全て見る</A><br>
-    　　　┣ <A href="/book/list.php?sc=1">男女・恋愛 </A><br>
-    　　　┣ <A href="/book/list.php?sc=2">愛・夫婦・結婚</A><br>
-    　　　┣ <A href="/book/list.php?sc=3">心・心理学、心の病</A><br>
-    　　　┣ <A href="/book/list.php?sc=4">セルフ・自分を見直す</A><br>
-    　　　┣ <A href="/book/list.php?sc=5">人間・人間関係</A><br>
-    　　　┣ <A href="/book/list.php?sc=6">人生・生き方</A><br>
-    　　　┣ <A href="/book/list.php?sc=7">癒し系 </A><br>
-    　　　┣ <A href="/book/list.php?sc=8">仕事・マーケティング </A><br>
-    　　　┣ <A href="/book/list.php?sc=9">ポジティブシンキング </A><br>　　　┣ <A href="/book/list.php?sc=10">成功法則・夢をかなえる</A><br>
-    　　　┣ <A href="/book/list.php?sc=11">考え方・ノウハウもの </A><br>
-    　　　┣ <A href="/book/list.php?sc=12">小説・ファンタジー</A><br>
-    　　　┣ <A href="/book/list.php?sc=13">病気・体</A><br>
-    　　　┣ <A href="/book/list.php?sc=14">詩集・哲学 </A><br>
-    　　　┣ <A href="/book/list.php?sc=15">古典・悟り</A><br>
-    　　　┣ <A href="/book/list.php?sc=16">エッセイ・ノンフィクション</A><br>
-    　　　┣ <A href="/book/list.php?sc=17">子供・家族</A><br>
-    　　　┣ <A href="/book/list.php?sc=18">社会の流れ・全般</A><br>
-    　　　┣ <A href="/book/list.php?sc=19">ことば集・教訓</A><br>
-    　　　┣ <A href="/book/list.php?sc=20">その他 </A><br>
-    　　　┗<A href="/book/list.php?sc=0">なし</A><br><br>
-
-    <img src=../img/point_ko.gif alt=width=22 height=17 border="0"><B>募集「ぜひ、教えてください！」</B><img src=../img/point_ko.gif
-                                                                                                 alt=width=22 height=17
-                                                                                                 border="0"><br><br>
-    　　　┣　<A href="/mail.php?title=%A4%A2%A4%CA%A4%BF%A4%CE%A1%D6%A4%AA%A4%B9%A4%B9%A4%E1%A4%CE%CB%DC%A1%D7%A4%F2%B6%B5%A4%A8%A4%C6%A4%AF%A4%C0%A4%B5%A4%A4%A1%A3%3Cbr%3E%A4%E8%A4%ED%A4%B7%A4%B1%A4%EC%A4%D0%CD%FD%CD%B3%A4%E2%B6%B5%A4%A8%A4%C6%A4%AF%A4%C0%A4%B5%A4%A4%A4%CD%A1%A3">あなたの「おすすめの本」</A><br>
-    　　　┗
-    <A href="/mail.php?title=%A4%A2%A4%CA%A4%BF%A4%CE%A1%D6%BF%CD%C0%B8%A4%F2%CA%D1%A4%A8%A4%BF%CB%DC%A1%D7%A4%F2%B6%B5%A4%A8%A4%C6%A4%AF%A4%C0%A4%B5%A4%A4%A1%A3%3Cbr%3E%A4%E8%A4%ED%A4%B7%A4%B1%A4%EC%A4%D0%BE%DC%A4%B7%A4%AF%B6%B5%A4%A8%A4%C6%A4%AF%A4%C0%A4%B5%A4%A4%A4%CD%A1%A3">あなたの「人生を変えた本」</A><br><br><br>
-
-
-    <?php require_once $_SERVER["DOCUMENT_ROOT"] . '/inc/foot_set_2column2.inc' ?>
+<?php require_once '/inc/foot_set_2column2.inc' ?>
